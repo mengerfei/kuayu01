4fa2951a9679295962659998dcba75840f73d398
mengerfei <txgzlove@gmail.com>
2020-06-29 15:08:26 +0800

    git 二次提交

--- meng-com/public/ming.js
+++ meng-com/public/ming.js
@@ -1,9 +1,3 @@
-const request = new XMLHttpRequest()
-request.open('GET', 'http://qq.com:8888/friends.json')
-request.onreadystatechange = () => {
-    if (request.readyState === 4 && request.status === 200) {
-        console.log(request.response)
-
-    }
-}
-request.send()
+const script = document.createElement('script')
+script.src = 'http://qq.com:8888/friends.js'
+document.body.appendChild(script)
--- qq-com/server.js
+++ qq-com/server.js
@@ -39,6 +39,15 @@ var server = http.createServer(function (request, response) {
         response.setHeader("Access-Control-Allow-Origin", "http://meng.com:9999")
         response.write(fs.readFileSync("./public/friends.json"))
         response.end();
+    } else if (path === '/friends.js') {
+        response.statusCode = 200;
+        response.setHeader("Content-Type", "text/javascript;charset=utf-8");
+        const string = fs.readFileSync("./public/friends.js").toString()
+        const data = fs.readFileSync("./public/friends.json").toString()
+        const string2 = string.replace("{{data}}", data)
+
+        response.write(string2)
+        response.end();
     } else {
         response.statusCode = 404;
         response.setHeader("Content-Type", "text/html;charset=utf-8");
